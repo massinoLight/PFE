a533437c92245cfaa750d36a41e1af45e7967d6b
Ram <ramziomari1995@gmail.com>
2020-08-05 19:34:24 +0100

    SceneForm marche bien, possibilité de rotation et placement, manque: ajouter les differents models, ajouter l'image du model choisi en bas,puis interface et fonctionnalité Gerant

--- app/src/main/java/com/massino/pfeadelramzi/SceneformKot.kt
+++ app/src/main/java/com/massino/pfeadelramzi/SceneformKot.kt
@@ -3,16 +3,18 @@ package com.massino.pfeadelramzi
 import android.net.Uri
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.ar.core.Anchor
-import com.google.ar.core.HitResult
-import com.google.ar.core.Plane
+import com.google.ar.core.*
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.FrameTime
+import com.google.ar.sceneform.Scene
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
+import android.graphics.Point
 
 
 class SceneformKot : AppCompatActivity() {
@@ -21,8 +23,8 @@ class SceneformKot : AppCompatActivity() {
     private lateinit var arFragment: ArFragment
 
     private val pointer = PointerDrawable()
-    private val isTracking = false
-    private val isHitting = false
+    private var isTracking = false
+    private var isHitting = false
     //private var fragment: ArFragment? = null
     private var modelLoader: ModelLoader? = null
     private val theAnchorNode: AnchorNode? = null
@@ -30,11 +32,15 @@ class SceneformKot : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sceneforme)
         arFragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
-
+        arFragment.arSceneView.scene
+            .addOnUpdateListener(Scene.OnUpdateListener { frameTime: FrameTime? ->
+                arFragment.onUpdate(frameTime)
+                onUpdate()
+            })
         arFragment?.setOnTapArPlaneListener { hitResult: HitResult, plane: Plane, motionEvent: MotionEvent ->
             val anchor = hitResult.createAnchor()
 
-            // int imageUrl = getIntent().getIntExtra("image_url",0);
+            //AJOUTER TOUT LES MODELS
             val imageUrl = intent.getStringExtra("image_url")
             Toast.makeText(this,imageUrl,Toast.LENGTH_LONG).show()
             placeObject(arFragment, anchor, Uri.parse("Bench.sfb"))
@@ -46,7 +52,7 @@ class SceneformKot : AppCompatActivity() {
         ModelRenderable.builder()
             .setSource(arFragment.context, uri)
             .build()
-            .thenAccept({ modelRenderable -> addNodeToScene(arFragment, anchor, modelRenderable) })
+            .thenAccept { modelRenderable -> addNodeToScene(arFragment, anchor, modelRenderable) }
             .exceptionally { throwable ->
                 Toast.makeText(arFragment.context, "Error:" + throwable.message, Toast.LENGTH_LONG)
                     .show()
@@ -62,6 +68,60 @@ class SceneformKot : AppCompatActivity() {
         node.setParent(anchorNode)
         arFragment.arSceneView.scene.addChild(anchorNode)
         node.select()
+    }
+
+    private fun onUpdate() {
+        val trackingChanged = updateTracking()
+        val contentView = findViewById<View>(android.R.id.content)
+        if (trackingChanged) {
+            if (isTracking) {
+                contentView.overlay.add(pointer)
+            } else {
+                contentView.overlay.remove(pointer)
+            }
+            contentView.invalidate()
+        }
+        if (isTracking) {
+            val hitTestChanged = updateHitTest()
+            if (hitTestChanged) {
+                pointer.isEnabled = isHitting
+                contentView.invalidate()
+            }
+        }
+    }
+
+    private fun updateTracking(): Boolean {
+        val frame: Frame? = arFragment.arSceneView.arFrame
+        val wasTracking = isTracking
+        isTracking = frame != null &&
+                frame.camera.trackingState == TrackingState.TRACKING
+        return isTracking != wasTracking
+    }
+
+    private fun updateHitTest(): Boolean {
+        val frame: Frame? = arFragment.arSceneView.arFrame
+        val pt: Point = getScreenCenter()
+        val hits: List<HitResult>
+        val wasHitting = isHitting
+        isHitting = false
+        if (frame != null) {
+            hits = frame.hitTest(pt.x.toFloat(), pt.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane &&
+                    trackable.isPoseInPolygon(hit.hitPose)
+                ) {
+                    isHitting = true
+                    break
+                }
+            }
+        }
+        return wasHitting != isHitting
+    }
+
+    private fun getScreenCenter(): Point {
+        val vw = findViewById<View>(android.R.id.content)
+        return Point(vw.width / 2, vw.height / 2)
     }
 
 
