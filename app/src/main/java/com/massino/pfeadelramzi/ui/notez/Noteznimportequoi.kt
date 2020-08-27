7cde0ae74ba21d49440c4fa37fb3d113ba9971b6
Ram <ramziomari1995@gmail.com>
2020-08-27 01:08:09 +0100

    RatingBar et Firebase

--- app/src/main/java/com/massino/pfeadelramzi/ui/notez/Noteznimportequoi.kt
+++ app/src/main/java/com/massino/pfeadelramzi/ui/notez/Noteznimportequoi.kt
@@ -1,23 +1,53 @@
 package com.massino.pfeadelramzi.ui.notez
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.massino.pfeadelramzi.*
+import android.widget.Button
+import android.widget.RatingBar
+import android.widget.RatingBar.OnRatingBarChangeListener
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.firebase.database.*
+import com.massino.pfeadelramzi.R
+
 
 class Noteznimportequoi : Fragment() {
+    var mdatabase : DatabaseReference?=null
 
-    private lateinit var viewModel: NoteznimportequoiViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProviders.of(this).get(NoteznimportequoiViewModel::class.java)
+
         val root = inflater.inflate(R.layout.noteznimportequoi_fragment, container, false)
+
+        val ratingBar: RatingBar = root.findViewById(R.id.ratingBar)
+        val setrating: Button = root.findViewById(R.id.setrating)
+
+        mdatabase = FirebaseDatabase.getInstance().reference.child("Rating")
+        setrating.setOnClickListener{
+            val msg= ratingBar.rating.toString()
+            Toast.makeText(context,"On vous remercie !",Toast.LENGTH_LONG).show()
+
+            ratingBar.onRatingBarChangeListener =
+                OnRatingBarChangeListener { ratingBar, rating, fromUser ->
+                    if (fromUser) mdatabase!!.setValue(rating);
+                }
+        }
+
+
+//        mdatabase!!.addValueEventListener(object : ValueEventListener{
+//            override fun onCancelled(error: DatabaseError) {
+//                TODO("Not yet implemented")
+//            }
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                TODO("Not yet implemented")
+//            }
+//
+//        })
         return root
     }
 
