035d93fce8538ae2138463f315f2a5ceaccd6a8e
Ram <ramziomari1995@gmail.com>
2020-09-21 07:48:12 +0100

    button deconnexion gerant

--- app/src/main/java/com/massino/pfeadelramzi/MenuPrincipalGerant.kt
+++ app/src/main/java/com/massino/pfeadelramzi/MenuPrincipalGerant.kt
@@ -4,9 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.activity_menu_principal_gerant.*
-import java.lang.Exception
+
 
 class MenuPrincipalGerant : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,5 +45,18 @@ class MenuPrincipalGerant : AppCompatActivity() {
         startActivity(intent7)
        // finish()
     }
+
+    fun deconnexionGerant(view: View) {
+            AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    val intent2 = Intent(this, MainActivity::class.java)
+                    startActivity(intent2)
+                    finish()
+                }
+
+
+        }
+
 
 }
